b1baf9d54896f96a9732fcc109c7ad8a5ca63762
mwuethr2 <michael.t.wuethrich@live.fr>
2016-03-25 19:34:16 +0100

    Added files via upload

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='MyWidgets',
+    version='0.0.1',
+    packages=['MyOrangeWidgets'],
+    entry_points={'orange.widgets': 'MyWidgets = MyOrangeWidgets'},
+    install_requires=[
+        'Orange>=2.1,<3',
+        'Orange-Textable>=1.5.2',
+    ],
+    author='University of Lausanne',
+    license='GNU General Public License v3 (GPLv3)',
+    keywords=[
+        '',
+        '',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2 :: only',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],    
+)
