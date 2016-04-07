262993a3944e0120d340cdd30082e822cb2b2194
mwuethr2 <michael.t.wuethrich@live.fr>
2016-04-07 10:59:56 +0200

    Update pythonSCript.py

--- pythonScripts/pythonSCript.py
+++ pythonScripts/pythonSCript.py
@@ -1,17 +1,19 @@
 print in_object.to_string()
 
-path = "C:/TreeTagger"
+path = "C:/TreeTagger" # chemin treetagger
 
 import subprocess as sp
-tmp = 'C:/Users/mwuethr2/Desktop/tmp_file.txt'
-f = open(tmp, 'w')
-f.write("hello\nworld\nin\nthe\nnew\nfile")
+
+tmp = 'C:/Users/mwuethr2/Desktop/tmp_file.txt' #creation un fichier temporaire
+f = open(tmp, 'w')#ouvre le fichier pour modifier
+f.write("hello\nworld\nin\nthe\nnew\nfile") #ecrire un mot ou caractere de ponctuation par ligne 
 f.close()
  
-commande = path + "/" + "bin/tag-english.bat"
+commande = path + "/" + "bin/tag-english.bat" #prend le chemin et y ajoute la commande
 print commande
-out = "C:/Users/mwuethr2/Desktop/out_file.txt"
-sp.call([commande, tmp, out ])
+out = "C:/Users/mwuethr2/Desktop/out_file.txt" #definie le chemin de l'output du fichier treetagger
+sp.call([commande, tmp, out ]) # commande = chemin du fichier d'input + chemin du fichier output
 
 f = open(out, 'r')
 print f.read()
+f.close()
