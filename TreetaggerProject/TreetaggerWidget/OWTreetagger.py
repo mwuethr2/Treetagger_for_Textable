1c9b55b4bffb723a7966532c14ce676d4ff8219f
xbarros <xav.ba@hotmail.com>
2016-05-13 01:48:42 +0200

    Update OWTreetagger.py

--- TreetaggerProject/TreetaggerWidget/OWTreetagger.py
+++ TreetaggerProject/TreetaggerWidget/OWTreetagger.py
@@ -4,7 +4,7 @@
 <icon>path_to_icon.svg</icon>
 <priority>11</priority> 
 """
-__version__ = u'0.1.0'
+
 # Standard imports...
 import Orange
 from OWWidget import *
@@ -46,20 +46,17 @@ class OWTreetagger(OWWidget):
         self.word_label = ""
         self.displayAdvancedSettings = False        
         self.label = u'tagged_data'
-        self.langues = ["english"]
+        self.langues = ['french']
+        
             
         # Always end Textable widget settings with the following 3 lines...
         self.uuid = None
         self.lien_ttgg = None
         self.loadSettings()
         self.uuid = getWidgetUuid(self)
+        self.lien_ttgg = None
 
-        
-        # Settings and other attribute initializations...
-
-
-
-            
+    
         # Other attributes...
         self.inputData = None   
         self.segmenter = Segmenter()
@@ -67,95 +64,31 @@ class OWTreetagger(OWWidget):
         
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------        
         
-        # Next two instructions are helpers from TextableUtils. Corresponding
-        # interface elements are declared here and actually drawn below (at
-        # their position in the UI)...
-        self.infoBox = InfoBox(widget=self.controlArea)
-        self.sendButton = SendButton(
-            widget=self.controlArea,
-            master=self,
-            callback=self.sendData,
-            infoBoxAttribute=u'infoBox',
-            sendIfPreCallback=self.updateGUI,
-        )
-
-        # The AdvancedSettings class, also from TextableUtils, facilitates
-        # the management of basic vs. advanced interface. An object from this 
-        # class (here assigned to self.advancedSettings) contains two lists 
-        # (basicWidgets and advanceWidgets), to which the corresponding
-        # widgetBoxes must be added.
-        self.advancedSettings = AdvancedSettings(
-            widget=self.controlArea,
-            master=self,
-            callback=self.advence,
-        )
-
-        # User interface...
-
-        # Advanced settings checkbox (basic/advanced interface will appear 
-        # immediately after it...
-        self.advancedSettings.draw()
-
-        self.advancedSettings.advancedWidgetsAppendSeparator()
-        
         #bouton pour trouver lien ttgg
         self.infoBox3 = OWGUI.widgetBox(
             self.controlArea, 
             u"")
             
+        OWGUI.widgetLabel( 
+            widget = self.infoBox3,              
+            label=" Rappel :\n\nle chemin pour retrouver Treetagger doit contenir"
+                  "5 fichier:\n- 'bin',\n- 'cmd',\n- 'INSTALL.txt',\n- "
+                  "'INSTALL.txt~',\n- 'lib',\n- 'README.txt' ",
+        )    
         OWGUI.button(
             widget=self.infoBox3,
             master=self,
-            label='lien vers treetagger',
+            label='Veuillez entrer le lien correct vers treetagger',
             callback = self.browse,
         )
-        
-
-        # infoBox1
-        self.infoBox1 = OWGUI.widgetBox(
-            self.controlArea, 
-            u"Language ", 
-            addSpace=True)
-            
-        self.langueBox = OWGUI.comboBox(
-            widget = self.infoBox1,
-            master = self, 
-            value='langue', 
-            items = self.langues,
-            )
-        
-        
-        # infoBox2 donne des info sur input et output
-        self.infoBox2 = OWGUI.widgetBox(self.controlArea, u"Temporaire ")
-        
-        self.infoLine = OWGUI.widgetLabel( 
-            widget = self.infoBox2,              
-            label='No input.',
-        )    
-        
-        
-
-
-        OWGUI.separator(widget=self.controlArea, height=3)
-
-        # From TextableUtils: a minimal Options box (only segmentation label).
-        basicOptionsBox = BasicOptionsBox(self.controlArea, self)
- 
-        OWGUI.separator(widget=self.controlArea, height=3)
-
-        # Now Info box and Send button must be drawn...
-        self.infoBox.draw()
-        self.sendButton.draw()
-        
-        # Send data if autoSend.
-        #self.sendButton.sendIf()
         
         # aller chercher le lien TreeTagger si pas deja la
         if self.lien_ttgg is None:
             self.browse()
         else:
-            self.cacher_bouton(True)
-            
+            self.initialiser_langue()
+            self.langues = self.langue_present()
+            self.afficher_interface(True)
         
 
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -196,22 +129,27 @@ class OWTreetagger(OWWidget):
     def browse(self):
         self.lien_ttgg = unicode(QFileDialog.getExistingDirectory(self, u'Entrer lien Treetagger'))
         
-        print self.lien_ttgg
+        
         if self.lien_ttgg == "" or not self.verifier_treetagger():
             #donne info sur le lien
-            self.browse()
+            QMessageBox.warning(
+                    None,
+                    'Textable',
+                    "Votre lien est incorrect, veuillez en selectionnez un nouveau !",
+                    QMessageBox.Ok
+            )
+            
+            self.afficher_interface(False)
+        else:
+            self.initialiser_langue()
+            self.afficher_interface(True)
         self.saveSettings()
 
 
     def verifier_treetagger(self):
-        #si reclique sur bouton et change lien desactiver bouton au cas ou lien faux
-        self.infoBox2.setDisabled(True)
-
-        # si le lien n'est pas trouvee
-        # self.infoLine1.setText("Le lien n'est pas trouve")
 
         # la liste qu'il devrait avoir
-        ttgg_list_folder = ['lib/french-abbreviations', 'cmd/utf8-tokenize.perl', 'bin/tree-tagger.exe' ]
+        ttgg_list_folder = ['lib/french.par', 'cmd/utf8-tokenize.perl', 'bin/tree-tagger.exe' ]
         #attention aux systemes d'explotataion (les fichiers changet (MAC WINDOWS))! import platform
 
         check = True
@@ -220,8 +158,89 @@ class OWTreetagger(OWWidget):
             if not check:
                 break
         return check
+        
+        
+    def initialiser_langue(self):
+        # initilise que les langues installees dans treetagger
+    
+        # va dans l'adresse rentre par l'utilisateur
+        os.chdir(self.lien_ttgg + "/lib")
+        
+        # la liste dans son dossier
+        langue_verification = os.listdir('.')
+        
+        langue_possible = ["french.par"]
+        self.langue_present = []
+
+        for i in xrange(len(langue_possible)):
+            if langue_possible[i] in langue_verification:
+                self.langue_present.append(langue_possible[0][:-4])
+                
+        
+    def afficher_interface(self, valeur):
+        
+        self.infoBox3.setVisible(True)
+
+        if valeur :
+            self.infoBox3.setVisible(False)
             
-       
+            # User interface...
+            
+            self.infoBox = InfoBox(widget=self.controlArea)
+            self.sendButton = SendButton(
+                widget=self.controlArea,
+                master=self,
+                callback=self.sendData,
+                infoBoxAttribute=u'infoBox',
+                sendIfPreCallback=self.updateGUI,
+            )
+
+            self.advancedSettings = AdvancedSettings(
+                widget=self.controlArea,
+                master=self,
+                callback=self.advence,
+            )
+
+            
+            self.advancedSettings.draw()
+
+            self.advancedSettings.advancedWidgetsAppendSeparator()
+            
+            
+            # infoBox1
+            self.infoBox1 = OWGUI.widgetBox(
+                self.controlArea, 
+                u"Language ", 
+                addSpace=True)
+                
+            self.langueBox = OWGUI.comboBox(
+                widget = self.infoBox1,
+                master = self, 
+                value='langue', 
+                items = self.langues,
+                )
+
+
+            OWGUI.separator(widget=self.controlArea, height=3)
+
+            # From TextableUtils: a minimal Options box (only segmentation label).
+            basicOptionsBox = BasicOptionsBox(self.controlArea, self)
+     
+            OWGUI.separator(widget=self.controlArea, height=3)
+
+            # Now Info box and Send button must be drawn...
+            self.infoBox.draw()
+            self.sendButton.draw()
+            
+            # Send data if autoSend.
+            self.sendButton.sendIf()
+            
+        
+        
+        # ajuster taille widjet
+        self.adjustSize()
+            
+        
     #recoit l'input
     def processInputData(self, inputData):
 
@@ -238,8 +257,6 @@ class OWTreetagger(OWWidget):
             #self.infoLine.setText('No input.')
             self.send('Text data', None)
         else:
-            #On efface les anciennes valeurs
-            self.clearCreatedInputs()
             #self.infoLine.setText(self.inputData.to_string())
             new_segmentations = list()
             for in_segment in self.inputData:
@@ -311,8 +328,8 @@ class OWTreetagger(OWWidget):
         f.write(inputData)
         f.close()
         
-        print self.langues
-        print self.langue
+        print 'kkk', self.langues
+        print 'kkk', self.langue
 
         commande = unicode( self.lien_ttgg + "/" + "bin/tag-" + self.langues[self.langue] + ".bat" )
         print commande
@@ -348,13 +365,7 @@ class OWTreetagger(OWWidget):
             out.append(outtmp[i].split('\t'))
         return [elem for elem in out if len(elem) == 3]
     
-    def cacher_bouton(self,valeur):
-        if valeur :
-            self.infoBox3.setVisible(False)
-            self.infoBox1.setDisabled(False)
-        else:
-            self.infoBox1.setDisabled(True)
-            
+    
             
     def updateGUI(self):
         """Update GUI state"""
@@ -362,46 +373,11 @@ class OWTreetagger(OWWidget):
             self.advancedSettings.setVisible(True)
         else:
             self.advancedSettings.setVisible(False)
-            
+    
+                
     def advence(self):
         pass
-    
-    def clearCreatedInputs(self):
-        """Delete all Input objects that have been created."""
-        # Delete strings...
-        for i in self.created_inputs:
-            i.clear()
-        # Empty list of created inputs.
-        del self.created_inputs[:]
-        # Delete those created inputs that are at the end of the string store.
-        for i in reversed(xrange(len(Segmentation.data))):
-            if Segmentation.data[i] is None:
-                Segmentation.data.pop(i)
-            else:
-                break 
-        print "effacer"
-    #fonctionne ?
-   
-    def onDeleteWidget(self):
-        """Free memory when widget is deleted (overriden method)"""
-        self.clearCreatedInputs()
-        print "Done"
-    
-    
-    def getSettings(self, *args, **kwargs):
-        """Read settings, taking into account version number (overriden)"""
-        settings = OWWidget.getSettings(self, *args, **kwargs)
-        settings["settingsDataVersion"] = __version__.split('.')[:2]
-        return settings
-
-    def setSettings(self, settings):
-        """Write settings, taking into account version number (overriden)"""
-        if settings.get("settingsDataVersion", None) \
-                == __version__.split('.')[:2]:
-            settings = settings.copy()
-            del settings["settingsDataVersion"]
-            OWWidget.setSettings(self, settings)
-    #----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 if __name__=='__main__':
     myApplication = QApplication(sys.argv)
